ed9e0f3694b060661b326c36f15439f171b445a5
diegomfv2007 <diegomfv2007@gmail.com>
2025-03-26 16:23:24 +0000

    second

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.diego.app',
   appName: 'diego',
-  webDir: 'dist/myapp'
+  webDir: 'dist/myapp/browser'
 };
 
 export default config;
