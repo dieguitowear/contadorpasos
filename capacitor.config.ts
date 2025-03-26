5ab9a1fd56ac8290bf83bb020fa7acd4206d0d32
diegomfv2007 <diegomfv2007@gmail.com>
2025-03-26 15:55:20 +0000

    contadorDiego

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.diego.app',
+  appName: 'diego',
+  webDir: 'dist/myapp'
+};
+
+export default config;
